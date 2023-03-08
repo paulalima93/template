3896ee5c5cc96cf4a41c461b81fe5b5ea03fcbcf
paulalima93 <91716314+paulalima93@users.noreply.github.com>
2023-03-08 14:18:20 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,50 +1,23 @@
-var bg, bgImg
-var bottomGround
-var topGround
-var balloon, balloonImg
+var sprite
+
 
 function preload(){
-bgImg = loadImage("assets/bg.png")
 
-balloonImg = loadAnimation("assets/balloon1.png","assets/balloon2.png","assets/balloon3.png")
 }
 
 function setup(){
+createCanvas(400,400)
+   
+sprite = createSprite(100,200,50,50);
 
-//imagem de plano de fundo
-bg = createSprite(165,485,1,1);
-bg.addImage(bgImg);
-bg.scale = 1.3
-
-//criando canto superior e inferior
-bottomGround = createSprite(200,390,800,20);
-bottomGround.visible = false;
-
-topGround = createSprite(200,10,800,20);
-topGround.visible = false;
-      
-//criando o balão     
-balloon = createSprite(100,200,20,50);
-balloon.addAnimation("balloon",balloonImg);
-balloon.scale = 0.2;
 
 
 
 }
 
 function draw() {
-  
   background("black");
-        
-          //fazendo o balão de ar quente pular
-          if(keyDown("space")) {
-            balloon.velocityY = -6 ;
-            
-          }
-
-          //adicionando gravidade
-           balloon.velocityY = balloon.velocityY + 2;
-   
-        drawSprites();
+      
+  drawSprites();
         
 }
